831a282d4ddb89f01b3bd0d240903c9f4acdb016
Marcus Mann <mpm7cf@virginia.edu>
2023-03-08 00:29:23 -0500

    finish

--- brightspot_interview/settings.py
+++ brightspot_interview/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -123,6 +124,12 @@ REST_FRAMEWORK = {
 REST_AUTH = {
     'USE_JWT': True,
     'JWT_AUTH_COOKIE': 'jwt-auth',
+    'JWT_EXPIRATION_DELTA': timedelta(days=2),
+}
+
+SIMPLE_JWT = {
+    # TODO: if time provides, implement refresh token behavior on frontend.
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=2),
 }
 
 # Internationalization
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='brightspot_interview',
+    version='v1.0.0',
+    packages=['blog', 'blog.migrations', 'brightspot_interview'],
+    url='https://github.com/the-mann/brightspot_interview',
+    license='',
+    author='marcusmann',
+    author_email='marcus@marcusmann.dev',
+    description=''
+)
